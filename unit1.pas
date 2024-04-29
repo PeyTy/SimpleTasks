d224f3f682400ecad58bec1035464ef02ed8721d
Miraculous Ladybugreport <3642643+PeyTy@users.noreply.github.com>
2024-04-29 18:37:11 +0300

    The Big Bang

---
+++ unit1.pas
@@ -0,0 +1,65 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Edit1: TEdit;
+    Panel1: TPanel;
+    Tasks: TCheckGroup;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if Edit1.Text = '' then exit;
+  Tasks.Items.Add(Edit1.Text);
+  Edit1.Text := '';
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var i, j:integer; undone:string;
+begin
+  undone := '';
+  j := 0;
+
+  for i := 0 to Tasks.ControlCount - 1 do begin
+    if Tasks.Checked[j] then begin
+      //Tasks.Checked[i] := false;
+      Tasks.Items.Delete(j);
+    end else begin
+      j := j + 1;
+      //undone := undone + #13#10 + Tasks.Items[i];
+    end;
+  end;
+
+  //Tasks.Items.Text := undone;
+  //  CheckGroup1.Controls[i].Enabled := CheckGroup1.Checked[i];
+end;
+
+end.
+
